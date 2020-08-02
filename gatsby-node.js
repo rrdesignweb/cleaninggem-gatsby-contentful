fd851d3e370f6d3b9c1053d234ebe6793c1812d4
Rowan Richardson <rr.designweb@gmail.com>
2020-08-02 11:43:23 +1000

    Ignore Order MiniCssExtractPlugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -250,3 +250,16 @@ module.exports.createPages = ({ actions, graphql }) => {
     getTech,
   ])
 }
+
+module.exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+      plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+}
