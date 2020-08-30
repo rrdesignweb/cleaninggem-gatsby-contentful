83149b9c72cc5287c867275f2e6a93461c0132b7
Rowan Richardson <rr.designweb@gmail.com>
2020-08-30 16:24:08 +1000

    Add map on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,12 +5,14 @@ import ContactForm from "../components/contact-form/index"
 import Header from "../components/header/index"
 import Footer from "../components/footer/index"
 import SEO from "../components/seo"
+import MapImage from "../images/melbourne-map@2x.png"
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact Page" />
     <Nav />
     <Header siteTitle="Contact Us" />
+    <div className="contact-map-image" style={{height:`340px`,backgroundPosition: `center center`, backgroundSize: `cover`, backgroundImage:`url(${MapImage})`}}></div>
     <ContactForm />
     <Footer />
   </Layout>
